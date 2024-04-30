086cea24ce01d609f8b6b18000f30703a3098545
Sebastian <sebastiangenorth@gmail.com>
2024-04-30 12:05:59 +0200

    minor adjustments to the HTML

--- src/components/InvestmentCalculator.jsx
+++ src/components/InvestmentCalculator.jsx
@@ -1,18 +1,43 @@
 export default function InvestmentCalculator()  {
-    return <div id="user-input" className="input-group">
-        <div>
-            <p className="label">INITIAL INVESTMENT</p>
-            <input id="initial" className="input" placeholder="0"/>
-            <p className="label">EXPECTED RETURNS</p>
-            <input id="expected" className="input"/>
+    return <section id="user-input">
+        <div  className="input-group">
+            <p>
+                <label>INITIAL INVESTMENT</label>
+                <input 
+                    id="initial" 
+                    className="input" 
+                    placeholder="0"
+                    type="number"
+                />
+            </p>
+            <p>
+                <label>EXPECTED RETURNS</label>
+                <input 
+                    id="expected" 
+                    className="input"
+                    type="number"
+                />
+            </p>
         </div>
        
-        <div>
-            <p  className="label">ANUAL INVESTMENT</p>
-            <input id="anual" className="input"/>
-            <p className="label">DURATION</p>
-            <input id="duration" className="input"/>
+        <div className="input-group">
+            <p>
+                <label>ANUAL INVESTMENT</label>
+                <input 
+                    id="anual" 
+                    className="input"
+                    type="number"
+                />
+            </p>
+            <p>
+                <label>DURATION</label>
+                <input 
+                    id="duration" 
+                    className="input"
+                    type="number"
+                />
+            </p>    
         </div>
          
-    </div>
+    </section>
 }
